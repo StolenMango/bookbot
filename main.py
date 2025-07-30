ae6b5fefc91696da8c68e9db1b663010bd492bb8
StolenMango <simionescuionut1@gmail.com>
2025-07-30 15:19:48 +0100

    sorting characters completed

--- main.py
+++ main.py
@@ -1,2 +1,3 @@
 from stats import count_words
 from stats import character_counter
+from stats import sort_on
--- stats.py
+++ stats.py
@@ -19,11 +19,29 @@ def character_counter(file_contents):
             characters_dict[char] = 1
     return characters_dict
 
+def sort_on(characters_dict):
+    list_of_dictionaries = []
+    for key, value in characters_dict.items():
+        list_of_dictionaries.append({"char": key, "num": value})
+    list_of_dictionaries.sort(key=lambda item: item["num"], reverse=True)
+    return list_of_dictionaries
+
 def main():
-    file_contents = get_book_text("books/frankenstein.txt")
+    path = "books/frankenstein.txt"
+    file_contents = get_book_text(path)
     number_of_words = count_words(file_contents)
-    characters = character_counter(file_contents)
-    print(characters)
-    print(f"{number_of_words} words found in the document")
+    characters_dict = character_counter(file_contents)
+    sorted_list = sort_on(characters_dict)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {number_of_words} total words")
+    print("--------- Character Count -------")
+    for item in sorted_list:
+        if item["char"].isalpha():
+            print(f"{item['char']}: {item['num']}")
+    print("============= END ===============")
+    # print(characters_dict)
+    # print(sorted_list)
 
 main()
